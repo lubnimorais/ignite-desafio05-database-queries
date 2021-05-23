4bfbc22e4160d80ffbb942a4d6b8e8fa2f5ea134
Lubni Morais <lubni.morais@gmail.com>
2021-05-23 00:29:17 -0300

    chore: Initial fix tests

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -15,16 +15,27 @@ export class GamesRepository implements IGamesRepository {
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
       .createQueryBuilder()
+      .select()
+      .where('title ILIKE LOWER(:param)', { param: `%${param}%` })
+      .getMany();
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT COUNT(*) AS count FROM games'); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
+    const games = await this.repository
+      .createQueryBuilder('game')
+      .innerJoin('game.users', 'user')
+      .where('game.id = :id', { id })
+      .getMany();
+
+      const users = games[0].users;
+
+      return users;
+      
       // Complete usando query builder
   }
 }
